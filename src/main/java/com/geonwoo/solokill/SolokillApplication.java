43537ab8818a9a0a7c5d4d62ab5a427315724fb9
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-04-11 23:24:45 +0900

    Initial commit

---
+++ src/main/java/com/geonwoo/solokill/SolokillApplication.java
@@ -0,0 +1,13 @@
+package com.geonwoo.solokill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolokillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolokillApplication.class, args);
+	}
+
+}
