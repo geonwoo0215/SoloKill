853f03f4e6ac2fd74874db80cc73d15842d2a8fd
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-04-12 01:56:29 +0900

    config(error) : ErrorCode 추가 - 에러 열거형 클래스로 Http 상태코드, 커스텀 코드, 메시지를 가집니다.

---
+++ src/main/java/com/geonwoo/solokill/global/exception/ErrorCode.java
@@ -0,0 +1,25 @@
+package com.geonwoo.solokill.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+
+@Getter
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+
+	// Common
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C001", "서버 에러입니다.");
+
+	private final HttpStatus status;
+	private final String code;
+	private final String message;
+
+	ErrorCode(HttpStatus status, String code, String message) {
+		this.status = status;
+		this.code = code;
+		this.message = message;
+	}
+}
