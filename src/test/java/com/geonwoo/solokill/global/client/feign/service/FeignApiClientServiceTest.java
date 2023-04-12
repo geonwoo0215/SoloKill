7fe33199cb80dfa8935996f93df252ee9a761b3a
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-04-13 01:31:19 +0900

    refactor(match): 매치 정보 저장 기능 테스트

--- src/test/java/com/geonwoo/solokill/global/client/feign/service/FeignApiClientServiceTest.java
+++ src/test/java/com/geonwoo/solokill/global/client/feign/service/FeignApiClientServiceTest.java
@@ -3,20 +3,42 @@ package com.geonwoo.solokill.global.client.feign.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.geonwoo.solokill.domain.match.dto.ChallengesResponse;
+import com.geonwoo.solokill.domain.match.dto.MatchInfo;
+import com.geonwoo.solokill.domain.match.dto.MatchResponse;
+import com.geonwoo.solokill.domain.match.dto.ParticipantResponse;
+import com.geonwoo.solokill.domain.summoner.converter.SummonerConverter;
 import com.geonwoo.solokill.domain.summoner.dto.SummonerInfoResponse;
-import com.geonwoo.solokill.global.client.service.ApiClientService;
+import com.geonwoo.solokill.domain.summoner.model.Summoner;
+import com.geonwoo.solokill.domain.summoner.repository.SummonerRepository;
+import com.geonwoo.solokill.global.client.feign.feignclient.RiotMatchOpenFeign;
+import com.geonwoo.solokill.global.client.feign.feignclient.RiotSummonerOpenFeign;
 
 @ExtendWith(MockitoExtension.class)
 class FeignApiClientServiceTest {
 
+	@InjectMocks
+	private FeignApiClientService feignApiClientService;
+
 	@Mock
-	private ApiClientService apiClientService;
+	private RiotSummonerOpenFeign riotSummonerOpenFeign;
+
+	@Mock
+	private RiotMatchOpenFeign riotMatchOpenFeign;
+
+	@Mock
+	private SummonerRepository summonerRepository;
 
 	@Test
 	@DisplayName("소환사 이름으로 소환사 정보를 호출한다.")
@@ -34,10 +56,23 @@ class FeignApiClientServiceTest {
 			.summonerLevel(344)
 			.build();
 
-		when(apiClientService.getSummonerInfoByName(name)).thenReturn(summonerInfoResponse);
+		Summoner summoner = Summoner.builder()
+			.id("id")
+			.accountId("accountId")
+			.puuid("puuid")
+			.name("리거누")
+			.profileIconId(1234)
+			.revisionDate(1234)
+			.summonerLevel(344)
+			.build();
+
+		MockedStatic<SummonerConverter> summonerConverterMockedStatic = mockStatic(SummonerConverter.class);
+		when(riotSummonerOpenFeign.getSummonerInfoByName(name)).thenReturn(summonerInfoResponse);
+		when(SummonerConverter.toSummoner(summonerInfoResponse)).thenReturn(summoner);
+		when(summonerRepository.save(summoner)).thenReturn(summoner);
 
 		//when
-		SummonerInfoResponse summonerInfoByName = apiClientService.getSummonerInfoByName(name);
+		SummonerInfoResponse summonerInfoByName = feignApiClientService.getSummonerInfoByName(name);
 
 		//then
 		assertThat(summonerInfoByName)
@@ -48,7 +83,40 @@ class FeignApiClientServiceTest {
 			.hasFieldOrPropertyWithValue("revisionDate", summonerInfoResponse.revisionDate())
 			.hasFieldOrPropertyWithValue("summonerLevel", summonerInfoResponse.summonerLevel());
 
-		verify(apiClientService).getSummonerInfoByName(name);
+		verify(riotSummonerOpenFeign).getSummonerInfoByName(name);
+		verify(summonerRepository).save(summoner);
+		summonerConverterMockedStatic.verify(() -> SummonerConverter.toSummoner(summonerInfoResponse), times(1));
 	}
 
+	@Test
+	@DisplayName("소환사의 puuid로 경기id를 조회하고, 경기id로 경기를 조회하여 사용자의 경기 기록과, 사용자 맞라인 상대의 경기 기록을 저장한다.")
+	void getMatchInfoByPuuid() {
+
+		String puuid = "puuid";
+		String gameType = "ranked";
+		Integer gameCount = 50;
+		String matchId = "matchId";
+		List<String> matchIds = new ArrayList<>();
+		matchIds.add(matchId);
+		when(riotMatchOpenFeign.getMatchId(puuid, gameType, gameCount)).thenReturn(matchIds);
+
+		ChallengesResponse challengesResponse = new ChallengesResponse(1);
+		ParticipantResponse participantResponse1 = new ParticipantResponse(puuid, 1, "TOP", "Jayce", challengesResponse,
+			1, 1, 1, 1, 1, 1, 1, 1, 1, true);
+		ParticipantResponse participantResponse2 = new ParticipantResponse("puuid2", 2, "TOP", "Gnar",
+			challengesResponse,
+			1, 1, 1, 1, 1, 1, 1, 1, 1, false);
+		List<ParticipantResponse> participants = new ArrayList<>();
+		participants.add(participantResponse1);
+		participants.add(participantResponse2);
+		MatchInfo matchInfo = new MatchInfo(participants);
+		MatchResponse matchResponse = new MatchResponse(matchInfo);
+
+		when(riotMatchOpenFeign.getMatchByMatchId(matchId)).thenReturn(matchResponse);
+
+		feignApiClientService.getMatchInfoByPuuid(puuid);
+
+		verify(riotMatchOpenFeign).getMatchId(puuid, gameType, gameCount);
+		verify(riotMatchOpenFeign).getMatchByMatchId(matchId);
+	}
 }
