5811f5ba9061a19b8b965408de5239913d7f831e
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-05-06 03:32:27 +0900

    feat(MatchRecord): 복합키 추가, 외래키 변경 - MatchRecord의 소환사 Pk와 경기Pk로 복합키를 생성합니다.

--- src/test/resources/sql/dummy.sql
+++ src/test/resources/sql/dummy.sql
@@ -4,10 +4,10 @@ values ('id', 'puuid', '리거누', 1234, 344);
 insert into match_info(id)
 values ('matchId');
 
-insert into match_record (id, team_id, puuid, team_position, champion_id, champion_name, solo_kills,
+insert into match_record (team_id, summoner_id, team_position, champion_id, champion_name, solo_kills,
                           vision_score, vision_wards_bought_in_game, total_minions_killed,
                           total_damage_dealt_to_champions,
                           gold_earned, kills, deaths, assists, win, match_info_id)
-values (1, 1, 'puuid', 'TOP', 1, 'Jayce', 1, 1, 1, 1, 1, 1, 1, 1, 1, true, 'matchId');
+values (1, 'summoner_id', 'TOP', 1, 'Jayce', 1, 1, 1, 1, 1, 1, 1, 1, 1, true, 'matchId');
 
 
