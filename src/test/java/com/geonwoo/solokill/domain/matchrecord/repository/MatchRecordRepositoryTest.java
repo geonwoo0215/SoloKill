41f380901657d1a7faee1cc475190886efc280df
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-07-08 17:54:22 +0900

    feat(test): dummy.sql 변경

--- src/test/java/com/geonwoo/solokill/domain/matchrecord/repository/MatchRecordRepositoryTest.java
+++ src/test/java/com/geonwoo/solokill/domain/matchrecord/repository/MatchRecordRepositoryTest.java
@@ -28,7 +28,7 @@ class MatchRecordRepositoryTest {
 	void findChampionNameByPuuid() {
 
 		//given
-		String id = "swsVftNdzs0OKJqJpbKwEtICk9_5oImBLO2Rdms-Dhzkcg";
+		String id = "id";
 
 		//when
 		Set<String> championNameById = matchRecordRepository.findChampionNameBySummonerId(id);
